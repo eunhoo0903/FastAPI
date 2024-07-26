be8cbede8acd4f4dcf5b96a91a24b331ac9e69b7
eunhoo0903 <eunhoo0903@naver.com>
2024-07-26 23:03:35 +0900

    경로 매개변수와 쿼리 매개변수

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/items/")
+def read_items(skip=0, limit=10):
+    return {"skip": skip, "limit": limit}
